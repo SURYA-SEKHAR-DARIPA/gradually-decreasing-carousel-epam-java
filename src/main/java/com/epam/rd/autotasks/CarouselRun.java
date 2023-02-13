98ddc71e13d7e2b38343ca7641b5ab4a468a5199
Surya <suryasekhardaripa60461@gmail.com>
2023-02-13 12:35:18 +0530

    done

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,80 @@
 package com.epam.rd.autotasks;
+import java.util.*;
 
 public class CarouselRun {
 
+    private final int[] container;
+    private int i = 0;
+    private int sum = 0;
+    private final int flag;
+    private int decrement = 1;
+
+    public CarouselRun (int[] container, int counter) {
+        this.container = Arrays.copyOf(container, container.length);
+        //i = counter;
+        flag = 0;
+        for (int elem : container ) {
+            sum += elem;
+        }
+    }
+
+    public CarouselRun (int[] container, int counter, int flag) {
+        this.container = Arrays.copyOf(container, container.length);
+        //i = counter;
+        this.flag = flag;
+        for (int elem : container ) {
+            sum += elem;
+        }
+    }
+
     public int next() {
-       throw new UnsupportedOperationException();
+       
+       int tempElem;
+        if (isFinished()) {     
+            return -1;
+        }
+        if (i == container.length) {
+            i = 0;
+            decrement++;
+        }
+        while (container[i] <= 0) {
+            i++;
+            if (i == container.length) {
+                i = 0;
+                decrement++;
+            }
+        }
+        if (flag == 0) {
+            sum--;
+            return container[i++]--;
+        } else if (flag == 1) {
+            tempElem = container[i];
+            container[i] /= 2;
+            sum = sum - container[i] - tempElem % 2;
+            i++;
+            return tempElem;
+        } else {
+            tempElem = container[i];
+            container[i] -= decrement;
+            System.out.println("decrement = " + decrement + " temp container = " + tempElem + " container = " + container[i]);
+            i++;
+            return tempElem;
+        }
+
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        if (flag == 2) {
+            for (int elem : container) {
+                if (elem > 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return sum == 0;
+        }
+
     }
 
 }
--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -1,7 +1,16 @@
 package com.epam.rd.autotasks;
+import java.util.*;
 
 public class GraduallyDecreasingCarousel extends DecrementingCarousel{
     public GraduallyDecreasingCarousel(final int capacity) {
         super(capacity);
+    }
+    public CarouselRun run(){
+        
+        if (getFlagRun() == 1) {
+            return null;
+        }
+        setFlagRun((byte) 1);
+        return new CarouselRun(getContainer(), getI(), 2);
     }
 }
